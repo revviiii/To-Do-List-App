5e142609a7b59986c3eea5962411042ebc5d82b9
caramoljc <joemcaitlyn24@gmail.com>
2025-09-22 14:48:40 +0800

    Fixed feature validation

--- ToDoApp.py
+++ ToDoApp.py
@@ -1,23 +1,25 @@
 tasks = []
 
 def add_task(task):
+    """Add a new task to the list"""
     tasks.append(task)
-    print("Task added!")
+    print("Task added successfully!")
 
 def show_tasks():
+    """Display all tasks"""
     if len(tasks) == 0:
-        print("No tasks yet")
+        print("No tasks added yet")
     else:
         for i in range (len(tasks)):
             print(i + 1, ".", tasks[i])
-
-def remove_task(taskNum):
-    if taskNum < 1 or taskNum > len(tasks):
-        print("Invalid task number!!")
-        return
+            
+def remove_task(task_num):
+    """Remove a task by its number"""
+    if task_num < 1 or task_num > len(tasks):
+        print("Invalid task number!")
     else:
-        tasks.pop(taskNum - 1) 
-        print("Task removed!!")
+        tasks.pop(task_num - 1) 
+        print("Task removed successfully!")
 
 def main():
     while True:
